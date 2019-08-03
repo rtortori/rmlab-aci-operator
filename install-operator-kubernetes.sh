8b64623598e5625dce9fd4731dfc0da5599027b2
Rick <rtortori@cisco.com>
2019-08-03 08:48:35 +0200

    fixed uninstall for OCP + warning on support matrix

--- install-operator-kubernetes.sh
+++ install-operator-kubernetes.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "This implementation is supported on the following platforms:"
+echo "Kubernetes v1.11.3+"
 
 # Deploy Kubernetes Prerequisites
 kubectl apply -f deploy/kubernetes/prereqs-k8s-aci-operator.yaml
--- uninstall-operator-openshift.sh
+++ uninstall-operator-openshift.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Delete CRD
-oc apply -f deploy/crds/rmlab_v1alpha1_acinamespace_crd.yaml
+oc delete -f deploy/crds/rmlab_v1alpha1_acinamespace_crd.yaml
 
 # Delete Kubernetes Operator
-oc apply -f deploy/openshift/deploy-ocp-aci-operator.yaml
+oc delete -f deploy/openshift/deploy-ocp-aci-operator.yaml
 
 # Delete Kubernetes Prerequisites
-oc apply -f deploy/openshift/prereqs-ocp-aci-operator.yaml
+oc delete -f deploy/openshift/prereqs-ocp-aci-operator.yaml
