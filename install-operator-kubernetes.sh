84e9ba4e32e2ee7024082b266534d9495283afbf
Rick <rtortori@cisco.com>
2019-08-03 08:16:02 +0200

    changed install scripts order of ops

--- install-operator-kubernetes.sh
+++ install-operator-kubernetes.sh
@@ -3,8 +3,8 @@
 # Deploy Kubernetes Prerequisites
 kubectl apply -f deploy/kubernetes/prereqs-k8s-aci-operator.yaml
 
-# Deploy Kubernetes Operator
-kubectl apply -f deploy/kubernetes/deploy-k8s-aci-operator.yaml
-
 # Deploy CRD
 kubectl apply -f deploy/crds/rmlab_v1alpha1_acinamespace_crd.yaml
+
+# Deploy Kubernetes Operator
+kubectl apply -f deploy/kubernetes/deploy-k8s-aci-operator.yaml
--- install-operator-openshift.sh
+++ install-operator-openshift.sh
@@ -3,8 +3,8 @@
 # Deploy OCP Prerequisites
 oc apply -f deploy/openshift/prereqs-ocp-aci-operator.yaml
 
-# Deploy OCP Operator
-oc apply -f deploy/openshift/deploy-ocp-aci-operator.yaml
-
 # Deploy CRD
 oc apply -f deploy/crds/rmlab_v1alpha1_acinamespace_crd.yaml
+
+# Deploy OCP Operator
+oc apply -f deploy/openshift/deploy-ocp-aci-operator.yaml
