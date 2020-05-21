211f1963e403ac8ec5ec4f68c53a90c8d3a86723
Rick <rtortori@cisco.com>
2020-05-21 17:54:54 +0200

    moved to stable CRD API, additional output when kubectl get aci

--- DEPRECATED-install-operator-kubernetes.sh
+++ DEPRECATED-install-operator-kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "WARNING!!!!"
 echo "This implementation is supported on the following platforms:"
-echo "Kubernetes 1.16 and above"
+echo "Kubernetes v1.11.3+ and Kubernetes < 1.16"
 read -p "Are you sure? Type Y or y to continue" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
@@ -12,7 +12,7 @@ fi
 kubectl apply -f deploy/kubernetes/prereqs-k8s-aci-operator.yaml
 
 # Deploy CRD
-kubectl apply -f deploy/crds/rmlab_v1alpha1_acinamespace_crd.yaml
+kubectl apply -f deploy/crds/DEPRECATED_rmlab_v1alpha1_acinamespace_crd.yaml
 
 # Deploy Kubernetes Operator
 kubectl apply -f deploy/kubernetes/deploy-k8s-aci-operator.yaml
